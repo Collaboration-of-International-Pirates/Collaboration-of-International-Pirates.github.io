b82eb68aa6299b87753979450c6c1cf1cd371eec
lucy-mothership <lucy@wearemothership.com>
2024-01-10 20:30:50 +0000

    Add 404 page and update navigation

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const ejs = require("gulp-ejs");
 const ejsCompiler = require("ejs");
 const rename = require("gulp-rename");
 const each = require("gulp-each");
@@ -18,7 +19,7 @@ const ARTICLES_PER_PAGE = 10
 function generateArticleHtmlPages(cb) {
     console.log("generateArticleHtmlPages");
     const md = new MarkdownIt();
-    let p = path.join(__dirname, "./src/views/articles/show.ejs");
+    let p = path.join(__dirname, "src/views/articles/show.ejs");
     const indexArticleTemplate = fs.readFileSync(p, "utf-8");
     return gulp
         .src("./src/articles/*.md") // Corrected the glob pattern
@@ -47,8 +48,8 @@ function generateArticleHtmlPages(cb) {
                     },
                 },
                 {
-                    views: [path.join(__dirname, "./src/views/")],
-                    root: path.join(__dirname, "./src/views"),
+                    views: [path.join(__dirname, "src/views/")],
+                    root: path.join(__dirname, "src/views"),
                 }
             );
             callback(null, renderedPage);
@@ -63,79 +64,91 @@ function generateArticleHtmlList(cb) {
     const articlesPath = path.join(__dirname, "/src/articles");
     const articleFilenames = fs.readdirSync(articlesPath);
     const indexArticlePath = path.join(
-      __dirname,
-      "src/views/articles/index.ejs"
+        __dirname,
+        "src/views/articles/index.ejs"
     );
     const indexArticleContent = fs.readFileSync(indexArticlePath, "utf-8");
     let articleDataList = [];
-  
+
     // Read each article file, extract metadata, and render Markdown to HTML
     articleFilenames.forEach((filename) => {
-      console.log(filename);
-      const filePath = path.join(__dirname, `./src/articles/${filename}`);
-      const fileContent = fs.readFileSync(filePath, "utf-8");
-      const { data, content } = matter(fileContent);
-      const htmlContent = md.render(content);
-      const articleSlug = filename.slice(0, -3);
-      // change the file extension from png,jpeg,gif,apng to webp
-      let convertImagesToWebP_imageUrl = data.imageUrl.replace(/\.(png|jpeg|gif|apng)/, ".webp");
-      const pageData = {
-        convertImagesToWebPImageUrl: data.convertImagesToWebPImageUrl,
-        imageUrl: data.imageUrl,
-        imageAlt: data.imageAlt,
-        title: data.title,
-        filename: articleSlug,
-        slug: data.slug,
-        summary: data.summary,
-        tags: data.keywords,
-        name: data.author[0].name,
-        datePublished: Date.parse(data.publishDate),
-        htmlContent,
-      };
-      articleDataList.push(pageData);
+        console.log(filename);
+        const filePath = path.join(__dirname, `./src/articles/${filename}`);
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+        const { data, content } = matter(fileContent);
+        const htmlContent = md.render(content);
+        const articleSlug = filename.slice(0, -3);
+        // change the file extension from png,jpeg,gif,apng to webp
+        const pageData = {
+            convertImagesToWebPImageUrl: data.convertImagesToWebPImageUrl,
+            imageUrl: data.imageUrl,
+            imageAlt: data.imageAlt,
+            title: data.title,
+            filename: articleSlug,
+            slug: data.slug,
+            summary: data.summary,
+            tags: data.keywords,
+            name: data.author[0].name,
+            datePublished: Date.parse(data.publishDate),
+            htmlContent,
+        };
+        articleDataList.push(pageData);
     });
     // Sort articles by datePublished in descending order
     articleDataList.sort((a, b) => b.datePublished - a.datePublished);
     // articleDataList = articleDataList.reverse();
     const totalPages = Math.ceil(articleDataList.length / ARTICLES_PER_PAGE);
     const htmlPagesPromises = [];
-  
+
     // Generate HTML pages with a subset of articles per page
     for (let page = 0; page < totalPages; page++) {
-      const articlesForCurrentPage = articleDataList.slice(
-        page * ARTICLES_PER_PAGE,
-        (page + 1) * ARTICLES_PER_PAGE
-      );
-  
-      // Render the EJS template with the article data
-      const renderedPage = ejsCompiler.render(
-        indexArticleContent,
-        {
-          articles: articlesForCurrentPage,
-          page,
-          hasNextPage: page < totalPages - 1,
-        },
-        {
-          views: [path.join(__dirname, "../src/views/")],
-          root: path.join(__dirname, "../src/views"),
-        }
-      );
-  
-      // Write the rendered page to an HTML file
-      const htmlFilePath = path.join(
-        __dirname,
-        "articles",
-        `${page}.html`
-      );
-      const writeHtmlPromise = fs.promises.writeFile(htmlFilePath, renderedPage);
-      htmlPagesPromises.push(writeHtmlPromise);
+        const articlesForCurrentPage = articleDataList.slice(
+            page * ARTICLES_PER_PAGE,
+            (page + 1) * ARTICLES_PER_PAGE
+        );
+
+        // Render the EJS template with the article data
+        const renderedPage = ejsCompiler.render(
+            indexArticleContent,
+            {
+                articles: articlesForCurrentPage,
+                page,
+                hasNextPage: page < totalPages - 1,
+            },
+            {
+                views: [path.join(__dirname, "src/views/")],
+                root: path.join(__dirname, "src/views"),
+            }
+        );
+
+        // Write the rendered page to an HTML file
+        const htmlFilePath = path.join(
+            __dirname,
+            "articles",
+            `${page}.html`
+        );
+        const writeHtmlPromise = fs.promises.writeFile(htmlFilePath, renderedPage);
+        htmlPagesPromises.push(writeHtmlPromise);
     }
-  
+
     // Wait for all HTML pages to be written before invoking the callback
     Promise.all(htmlPagesPromises)
-      .then(() => cb(null))
-      .catch((error) => cb(error));
-  }
-  
+        .then(() => cb(null))
+        .catch((error) => cb(error));
+}
 
-exports.build = gulp.series(generateArticleHtmlPages,generateArticleHtmlList);
+
+function generatePathsPage(cb) {
+    console.log("generatePaths_page");
+    
+    return gulp.src("src/views/page/*.ejs")
+        .pipe(ejs({}, {
+            views: [path.join(__dirname, "./src/views")],
+            root: path.join(__dirname, "./src/views"),
+        }))
+        .pipe(rename({ extname: ".html" }))
+        .pipe(gulp.dest("."));
+}
+
+
+exports.build = gulp.series(generateArticleHtmlPages, generateArticleHtmlList, generatePathsPage);
